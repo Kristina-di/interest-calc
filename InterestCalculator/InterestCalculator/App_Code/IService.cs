b79bd172b1a184138ff20d39f3637daea1cd1d5f
Kristina-di <62858565+Kristina-di@users.noreply.github.com>
2021-06-04 22:27:54 -0400

    Add files via upload

---
+++ InterestCalculator/InterestCalculator/App_Code/IService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IInterestCalculations
+{
+
+	[OperationContract]
+	decimal SimpleInterest (decimal _p, decimal _r, decimal _t);
+
+	[OperationContract]
+    decimal SimplePlusPrincipalInterest(decimal _p, decimal _r, decimal _t);
+
+	[OperationContract]
+    decimal CompoundInterest(decimal _p, decimal _r, decimal _n, decimal _t);
+
+	[OperationContract]
+    decimal PeriodicCompoundInterest(decimal _p, decimal _r, decimal _t);
+
+	[OperationContract]
+    AllInterest CalculateAll(decimal _p, decimal _r, decimal _t, decimal _n);
+
+	// TODO: Add your service operations here
+}
+
+
+
+// Use a data contract as illustrated in the sample below to add composite types to service operations.
+[DataContract]
+public class AllInterest
+{
+    public AllInterest(decimal simpleInterest, decimal simplePlusPrincipalInterest, decimal compoundInterest, decimal periodicCompoundInteres)
+    {
+        SimpleInterest = simpleInterest;
+        SimplePlusPrincipalInterest = simplePlusPrincipalInterest;
+        CompoundInterest = compoundInterest;
+        PeriodicCompoundInterest = periodicCompoundInteres;
+    }
+
+    [DataMember]
+    private decimal SimpleInterest { get; set; }
+
+    [DataMember]
+    private decimal SimplePlusPrincipalInterest { get; set; }
+
+    [DataMember]
+    private decimal CompoundInterest { get; set; }
+
+    [DataMember]
+    private decimal PeriodicCompoundInterest { get; set; }
+
+}
---
+++ InterestCalculator/InterestCalculator/App_Code/Service.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IInterestCalculations
+{
+    public decimal SimpleInterest(decimal _p, decimal _r, decimal _t)
+    {
+        
+        return (_p * _r * _t);
+        
+    }
+
+    public decimal SimplePlusPrincipalInterest(decimal _p, decimal _r, decimal _t)
+    {
+        
+        return (_p * (1 + _r * _t));
+        
+    }
+
+    public decimal CompoundInterest(decimal _p, decimal _r, decimal _n, decimal _t)
+    {
+        
+        return (_p * (1 + _r / _n));
+        
+    }
+
+    public decimal PeriodicCompoundInterest(decimal _p, decimal _r, decimal _t)
+    {
+        
+        return (_p * (1 + _r));
+        
+    }
+
+    public AllInterest CalculateAll(decimal _p, decimal _r, decimal _t, decimal _n)
+    {
+        
+
+
+         decimal SimpleInterest;
+         decimal SimplePlusPrincipalInterest;
+         decimal CompoundInterest;
+         decimal PeriodicCompoundInterest;
+
+         SimpleInterest = (_p * _r * _t);
+         SimplePlusPrincipalInterest = (_p * (1 + _r * _t));
+         CompoundInterest = (_p * (1 + _r / _n));
+         PeriodicCompoundInterest = (_p *(1 + _r));
+
+         var allInterest = new AllInterest(SimpleInterest, SimplePlusPrincipalInterest, CompoundInterest, PeriodicCompoundInterest);
+
+         return allInterest;
+
+    }
+
+   
+}
